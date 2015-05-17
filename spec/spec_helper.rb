fe7e2388aaf49a0e5bd3087e9667f32f95562078
Sean Callan <seancallan@gmail.com>
2015-05-17 11:51:25 -0700

    More complete spec helper with test coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,22 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+ENV['RACK_ENV'] = 'test'
+
+require 'coveralls'
+require 'simplecov'
+
+SimpleCov.formatters = [SimpleCov::Formatter::HTMLFormatter, Coveralls::SimpleCov::Formatter]
+
+SimpleCov.start do
+  add_filter 'spec'
+  coverage_dir 'docs/coverage'
+end
+
 require 'omniauth/pge'
+
+Dir['spec/support/**/*.rb'].each { |f| require f }
+
+RSpec.configure do |config|
+  config.pattern = '**/*_spec.rb'
+  config.mock_framework = :rspec
+end
