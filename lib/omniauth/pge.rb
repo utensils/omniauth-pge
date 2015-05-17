aa03864eb292e6e6a6c28569312e517a7e2a4942
Sean Callan <seancallan@gmail.com>
2015-05-17 00:20:30 -0700

    Generated gem

---
+++ lib/omniauth/pge.rb
@@ -0,0 +1,7 @@
+require "omniauth/pge/version"
+
+module Omniauth
+  module Pge
+    # Your code goes here...
+  end
+end
---
+++ lib/omniauth/pge/version.rb
@@ -0,0 +1,5 @@
+module Omniauth
+  module Pge
+    VERSION = "0.1.0"
+  end
+end
